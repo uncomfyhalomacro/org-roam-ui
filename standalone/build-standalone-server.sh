0e2b0620ef8724ae0e226ce307a6e78a9e464ec0
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2023-07-20 20:49:38 +0800

    update script

--- standalone/build-standalone-server.sh
+++ standalone/build-standalone-server.sh
@@ -12,6 +12,8 @@ patch ../pages/index.tsx < index.tsx.patch
 patch ../util/uniorg.tsx < uniorg.tsx.patch
 patch ../components/Sidebar/Link.tsx < Link.tsx.patch
 patch ../components/Sidebar/OrgImage.tsx < OrgImage.tsx.patch
+patch ../components/Tweaks/Behavior/BehaviorPanel.tsx < BehaviorPanel.tsx.patch
+patch ../components/contextmenu.tsx < contextmenu.tsx.patch
 
 # Copy previously exported content
 cp $1/graphdata.json ../
@@ -31,6 +33,9 @@ patch -R ../pages/index.tsx < index.tsx.patch
 patch -R ../util/uniorg.tsx < uniorg.tsx.patch
 patch -R ../components/Sidebar/Link.tsx < Link.tsx.patch
 patch -R ../components/Sidebar/OrgImage.tsx < OrgImage.tsx.patch
+patch -R ../components/Tweaks/Behavior/BehaviorPanel.tsx < BehaviorPanel.tsx.patch
+patch -R ../components/contextmenu.tsx < contextmenu.tsx.patch
+
 # Cleanup temporary data
 rm  ../graphdata.json
 rm -r ../public/notes
